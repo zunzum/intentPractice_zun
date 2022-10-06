8aea71ea2db006c2820e99f3382cf3e6047487e7
zunzum <zunzum@naver.com>
2022-10-06 21:18:24 +0900

    finish이용 현재화면 종료, 복귀

--- app/src/main/java/com/zunzum/intentpractice_zun/OtherActivity.kt
+++ app/src/main/java/com/zunzum/intentpractice_zun/OtherActivity.kt
@@ -1,11 +1,19 @@
 package com.zunzum.intentpractice_zun
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_other.*
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+
+        returnToMainBtn.setOnClickListener {
+//            val myIntent = Intent(this, MainActivity::class.java)
+//            startActivity(myIntent)
+            finish()
+        }
     }
 }
