547545ae1cd6fcfde557ad2573ec0aa7185c3de8
zunzum <zunzum@naver.com>
2022-10-06 21:13:39 +0900

    다른화면 추가 - ui작업 진행

--- app/src/main/java/com/zunzum/intentpractice_zun/MainActivity.kt
+++ app/src/main/java/com/zunzum/intentpractice_zun/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.zunzum.intentpractice_zun
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        moveToOtherBtn.setOnClickListener {
+            val myIntent = Intent(this,OtherActivity::class.java)
+            startActivity(myIntent)
+        }
     }
 }
