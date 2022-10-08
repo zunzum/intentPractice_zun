5a3eca2e4c3e5889ff2740db39ff6e4ddbf9445c
zunzum <zunzum@naver.com>
2022-10-08 21:03:20 +0900

    결과를 받아 돌아온 화면에서 새 닉네임 반영

--- app/src/main/java/com/zunzum/intentpractice_zun/MainActivity.kt
+++ app/src/main/java/com/zunzum/intentpractice_zun/MainActivity.kt
@@ -31,4 +31,17 @@ class MainActivity : AppCompatActivity() {
             startActivity(myIntent)
         }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        //돌아온 이유가 닉네임을 받으러 다녀온게 맞는지?
+        if (requestCode == REQUEST_FOR_NICKNAM) {
+            //추가질문 : 확인을 눌러서 돌아온게 맞는지?
+            if(resultCode == RESULT_OK) {
+                //실제 첨부된 새 닉네임을 꺼내서 텍스트뷰에 반영
+                val newNickname = data?.getStringExtra("ncikname")
+                nicknameTxt.text = newNickname
+            }
+        }
+    }
 }
