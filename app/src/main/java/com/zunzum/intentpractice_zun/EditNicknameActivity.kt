f8e9ac65d1590d2e267f4dba6e7c7a9a395c824f
zunzum <zunzum@naver.com>
2022-10-08 20:36:02 +0900

    닉네임 변경을 위한 화면 이동 (startActivityForResult)활용

--- app/src/main/java/com/zunzum/intentpractice_zun/EditNicknameActivity.kt
+++ app/src/main/java/com/zunzum/intentpractice_zun/EditNicknameActivity.kt
@@ -7,5 +7,6 @@ class EditNicknameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_nickname)
+
     }
 }
--- app/src/main/java/com/zunzum/intentpractice_zun/MainActivity.kt
+++ app/src/main/java/com/zunzum/intentpractice_zun/MainActivity.kt
@@ -6,9 +6,17 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val REQUEST_FOR_NICKNAM = 1005
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        editNicknameBtn.setOnClickListener {
+            val myIntent = Intent(this,EditNicknameActivity::class.java)
+            startActivityForResult(myIntent, REQUEST_FOR_NICKNAM)
+        }
 
 
         sendMessageBtn.setOnClickListener {
