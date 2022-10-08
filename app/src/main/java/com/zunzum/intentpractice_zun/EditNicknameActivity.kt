4fa441f71ba5a327fd28d2ef82ccde936fd0c5c1
zunzum <zunzum@naver.com>
2022-10-08 20:54:51 +0900

    닉네임 변경 화면 -> 입력 후 온료 클릭시 데이터 들고 돌아가기
    돌아간 메인화면에서 데이터 반영 별도 작업 필요

--- app/src/main/java/com/zunzum/intentpractice_zun/EditNicknameActivity.kt
+++ app/src/main/java/com/zunzum/intentpractice_zun/EditNicknameActivity.kt
@@ -1,12 +1,30 @@
 package com.zunzum.intentpractice_zun
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nickname.*
 
 class EditNicknameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_nickname)
+        okBtn.setOnClickListener {
+            //입력한 새 닉네임은 뭔지 변수로 저장 inputNewNickname이 변수이름
+            val inputNewNickname = newNicknameEdt.text.toString()
+
+            //입력한 닉네임을 가지고 메인으로 복귀
+
+            //아래는 입력한 닉네임을 담아주기 위한 용도로만 사용하는 Intent
+            val resultIntent = Intent()
+            resultIntent.putExtra("ncikname", inputNewNickname)
+            setResult(Activity.RESULT_OK, resultIntent)
+            finish()
+
+
+        }
+
 
     }
 }
